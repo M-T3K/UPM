1cf19d430d2e1e346e6adffcfc2baff757a70a8c
Alai <kevinalb12@gmail.com>
2017-10-06 11:38:50 +0200

    Completado :D

--- Programacion I/UE1/Recursion.java
+++ Programacion I/UE1/Recursion.java
@@ -1,7 +1,7 @@
 
 public class Recursion {
 
-	
+	// #Region: Ejercicios de Clase
 	
 	//-------------------------------------------------------------------
 	public static int productoEntre(int x, int y)	{
@@ -38,7 +38,73 @@ public class Recursion {
 			
 	}
 	//-------------------------------------------------------------------
+		
+	public static int factorialRec(int n)	{
+		
+		if(n < 1)	{
+			
+			return n;
+			
+		}
+		
+		return n * factorialRec(n - 1);
+		
+	}
+	//---------------------------------------------------------------
 	
+	public static int numeroCifras(int n)	{
+		
+		
+		if(n < 1)	{
+			
+			return 0;
+			
+		}
+		
+		return 1 + numeroCifras(n / 10);
+		
+	}
+	//---------------------------------------------------------------
+	
+	public static int sumaCifras(int n)	{
+		
+		if(n < 1)	{
+			
+			return 0;
+			
+		}
+		
+		return n % 10 + sumaCifras(n / 10);
+		
+	}
+	//---------------------------------------------------------------
+	
+	public static int sumaCifrasPares(int n)	{
+		
+		if(n < 1)	{
+			
+			return 0;
+			
+		}
+		return ((n % 10) % 2 == 0) ?  n % 10 + sumaCifrasPares(n / 10) : sumaCifrasPares(n / 10);
+	}
+	//---------------------------------------------------------------
+	
+	public static int sumaCifrasParesPosImpar(int n)	{
+		
+		if(n < 1)	{
+			
+			return 0;
+			
+		}	else if(((n % 10) % 2 == 0) && (numeroCifras(n) % 2 == 0))	{
+			
+			return n % 10 + sumaCifrasParesPosImpar(n / 10);
+			
+		}
+		return sumaCifrasParesPosImpar(n / 10);
+		
+	}
+	//---------------------------------------------------------------
 	
 	public static void main(String[] args)	{
 		
@@ -47,6 +113,13 @@ public class Recursion {
 		System.out.println(sumaEntre(2, 8));
 		System.out.println(sumaCuadradosEntre(2, 8));
 		System.out.println(sumaCuadradosParesEntre(2, 8));
+		System.out.println("////////");
+		System.out.println(factorialRec(6));
+		System.out.println(numeroCifras(1234));
+		System.out.println(sumaCifras(1234));
+		System.out.println(sumaCifrasPares(1234));
+		System.out.println(sumaCifrasParesPosImpar(12345521));
+		
 		
 	}
 }
