4d098268e0c52a5fd174840921c126a985fcd48b
Prince Kiwii <kevinalb12@gmail.com>
2018-09-15 23:00:32 +0200

    Update Utils.java

--- AlgoritmosEstructurasDeDatos/EjercicioIndividual1/Utils.java
+++ AlgoritmosEstructurasDeDatos/EjercicioIndividual1/Utils.java
@@ -1,7 +1,13 @@
 package aed.loops;
 
 public class Utils {
-	
+  
+  /**
+   * 
+   * @param a : Array de 'Integer'
+   * @param elem : Elemento del tipo 'Integer' 
+   * @return La cantidad maxima de veces en las que 'elem' aparece en 'a' consecutivamente
+   */
   public static int maxNumRepeated(Integer[] a, Integer elem)  {
 
     int currRep = 0; // Contador 
