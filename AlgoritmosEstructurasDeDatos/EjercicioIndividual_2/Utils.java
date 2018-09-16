fec40c389c9c5bbb0b7dbc31fc516a74bdea3c28
Prince Kiwii <kevinalb12@gmail.com>
2018-09-16 12:01:05 +0200

    Update Utils.java

--- AlgoritmosEstructurasDeDatos/EjercicioIndividual_2/Utils.java
+++ AlgoritmosEstructurasDeDatos/EjercicioIndividual_2/Utils.java
@@ -4,35 +4,50 @@ import java.awt.List;
 import es.upm.aedlib.indexedlist.*;
 
 public class Utils {
-	
+  
+  /**
+   * 
+   * @param l : Lista del tipo 'IndexedList'
+   * @return Una Lista del tipo 'IndexedList' sin elementos repetidos
+   */
   public static <E> IndexedList<E> deleteRepeated(IndexedList<E> l) {
       
     IndexedList<E> list = new ArrayIndexedList<E>();
 
     for(E elem : l) {
 
-      boolean found = false;
-      for(int i = 0; i < list.size() && !found; ++i) {
-
-        if(elem.equals(list.get(i))) {
-
-          found = true;
-
-        }
-
-      }
-
-      if(!found) {
+      if(!find(elem, list)) {
 
         list.add(list.size(), elem);
 
       }
 
-      found = false;
-
     }
 
     return list;
+
+  }
+
+  /**
+   * 
+   * @param elem : Elemento de tipo 'elem' (generic) que estamos buscando.
+   * @param list : Lista del tipo "IndexedList" en la que estamos buscando.
+   * @return boolean 'true' si elem existe en la lista, 'false' si no
+   */
+  private static <E> boolean find(E elem, IndexedList<E> list) {
+    
+    boolean found = false;
+    for(int i = 0; i < list.size() && !found; ++i) {
+
+      if(elem.equals(list.get(i))) {
+
+        found = true;
+
+      }
+
+    }
+
+    return found;
 
   }
 
